05183c3d7e7c3fc45817989a5ebdcabd22cedab6
Hua Song <shcalm@gmail.com>
2015-08-28 17:23:00 +0800

    Update views.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,19 @@
 from . import main
 from flask import render_template
+from app import db
 
 @main.route('/')
 def index():
 	return render_template('index.html')
 
+@main.route('/initdb')
+def init_db():
+    db.create_all()
+    return 'db is inited'
+
+@main.route('/deldb')
+def del_post():
+    engine = db.get_engine(db.get_app())
+    post_up.drop(engine)
+
+    return 'post_up is del'
